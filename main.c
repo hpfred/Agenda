974b3f26e52398b0b40a9368549285c0cb290446
hpfred <frepantunes@yahoo.com.br>
2019-03-31 20:46:58 -0300

    Primeiro Commit. Apenas texto do menu e definição das funções.

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void Incluir();
+void Apagar();
+void Buscar();
+void Listar();
+
+main(){
+    void
+    *pBuffer;
+
+    printf("--Menu--\n\t1. Incluir nome\n\t2. Remover nome\n\t3. Buscar nome\n\t4. Listar nomes");
+
+    return 0;
+}
+
+void Incluir();
+void Apagar();
+void Buscar();
+void Listar();
